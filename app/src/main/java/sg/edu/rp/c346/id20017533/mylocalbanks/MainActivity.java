497e6a2ba651c7bba080827768c21c84be7326f3
20017533 <20017533@myrp.edu.sg>
2021-05-27 16:53:51 +0800

    update version

--- app/src/main/java/sg/edu/rp/c346/id20017533/mylocalbanks/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20017533/mylocalbanks/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_language, menu);
         return true;
     }
     public boolean onContextItemSelected(MenuItem item) {
